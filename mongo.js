c440e7709379c2c5e1ea56cf3aae19e6992165a7
BryceKrispy9 <brycepearson09@gmail.com>
2021-07-26 21:32:59 -0600

    commit on master

--- mongo.js
+++ mongo.js
@@ -1,18 +1,49 @@
-// db.createUser({
-//     user: 'Devyn',
-//     pwd: 'password',
-//     customData: { startDate: new Date() },
-//     roles: [
-//       { role: 'clusterAdmin', db: 'admin' },
-//       { role: 'readAnyDatabase', db: 'admin' },
-//       'readWrite'
-//     ]
-//   })
+db.createUser({
+    user: 'Devyn',
+    pwd: 'password',
+    customData: { startDate: new Date() },
+    roles: [
+      { role: 'clusterAdmin', db: 'admin' },
+      { role: 'readAnyDatabase', db: 'admin' },
+      'readWrite'
+    ]
+  })
 
-// db.books.insert({
-//     "title": "OOP Programming",
-//     "startDate": new Date(),
-//     "authors": [
-//         { "name": "Jon Snow Jr" }
-//     ]
-// })
+
+
+db.books.insert({
+    "title": "OOP Programming",
+    "startDate": new Date(),
+    "authors": [
+        { "name": "Jon Snow Jr" }
+    ]
+})
+
+
+
+db.books.insertMany([
+  {
+    "name": "Confident Ruby",
+    "publishDate": new Date(),
+    "authors": [
+      { "name": "Avdi Grimm" }
+    ]
+  },
+  {
+    "name": "The War of Art",
+    "publishedDate": new Date(),
+    "authors": [
+      { "name": "Steven Pressfield" }
+    ]
+  },
+  {
+    "name": "Blink",
+    "publishedDate": new Date(),
+    "authors": [
+      { "name": "Malcolm Gladwell" }
+    ]
+  }
+])
+
+
+db.books.find().pretty()
